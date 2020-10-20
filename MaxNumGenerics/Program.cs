546d176a16f967221c84701d9d1b5e6d9e82b399
Akash <akashmane12598@gmail.com>
2020-10-20 23:51:54 +0530

    [Akash] Add. Master Code

---
+++ MaxNumGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxNumGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics Code!");
+        }
+    }
+}
