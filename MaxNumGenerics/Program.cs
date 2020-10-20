ef6e3e9fccf3f31fdc4af520607958db26581bf4
Akash <akashmane12598@gmail.com>
2020-10-21 00:01:21 +0530

    [Akash] Refactor Master

--- MaxNumGenerics/Program.cs
+++ MaxNumGenerics/Program.cs
@@ -4,7 +4,7 @@ namespace MaxNumGenerics
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generics Code!");
         }
